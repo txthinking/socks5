7d82196384693b7f4ecc405dba9c06683fa06d40
txthinking <cloud@txthinking.com>
2020-03-22 14:18:38 +0800

    readme

--- example_test.go
+++ example_test.go
@@ -2,7 +2,7 @@ package socks5_test
 
 import "github.com/txthinking/socks5"
 
-func ExampleStandardSocks5Server() {
+func ExampleServer() {
 	s, err := socks5.NewClassicServer("127.0.0.1:1080", "127.0.0.1", "", "", 60, 0, 60, 60)
 	if err != nil {
 		panic(err)
